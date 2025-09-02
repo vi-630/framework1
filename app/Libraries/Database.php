fd66904da8475d2c879a4716a1bafa0b1054cbd2
vi-630 <vitoriadiasne@gmail.com>
2025-09-02 09:33:17 -0300

    atualizacao database

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -15,14 +15,15 @@ class Database {
             PDO::ATTR_PERSISTENR => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 
-            try {
+        ];
+            try{
                 //cria a instancapo do pdo
                 $this->$dbh = new PDO($dbh,$this->usuario, $this->senha,$opcoes);
             } catch (PDOException $e) {
                 print "Error!: ".$e->getMenssage()."<br/>";
                 die();
             }//fim do catch
-        ];
+        
     }//fim do construtor
 
     //prepare Statements com query
@@ -33,7 +34,7 @@ class Database {
 
     //vincula um valor ao parametro
     public function bind($parametro, $valor, $tipo = null){
-        if(is_null($tipo)){
+        if(is_null($tipo)):
             switch(true){
                 case is_int($valor):
                     $tipo = PDO::PARAM_INT;
@@ -47,17 +48,35 @@ class Database {
                 default:
                     $tipo = PDO::PARAM_STR;
             }//fim do switch
-        }//fim da função bind
         endif;
         $this->stmt->bindValue($parametro, $valor, $tipo);
     }//fim da funcao bind
 
     //executa prepared statement
     public function executa(){
-        return $this->stmt->executa()
+        return $this->stmt->execute();
     }//fim da função executa
 
+    //obtem um unico registro
+    public function resultado(){
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }//fim da função resultado
 
+    //obtem um confundo de registro
+    public function resultados(){
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }//fim da função resultados
 
-}
+    //retorna o numero de linha afetadas pela ultima instrução sql
+    public function totalResultados(){
+        return $this->stmt->rowCount();
+    }//fim da função
+
+    //retorna o ultimo id inserido no banco de dados
+    public function ultimoIdInserido(){
+        return $this->dbh->lastInsertId();
+    }//fim da função ultimoIdInserido
+}//fim da classe Database
 ?>
