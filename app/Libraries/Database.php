fbb0bce44e83ba9ee6ad891f32dd5ee812261419
vi-630 <vitoriadiasne@gmail.com>
2025-09-02 08:32:53 -0300

    SLA

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -4,8 +4,8 @@ class Database {
     private $usuario = 'root';
     private $senha = '';
     private $banco = 'framework';
+    private $porta = '3307';
     private $dbh;
-
 
     public function _construct(){
         //fonte de dados ou DNS contém as informações necessárias para conectar ao banco de dados
@@ -16,6 +16,7 @@ class Database {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 
             try {
+                //cria a instancapo do pdo
                 $this->$dbh = new PDO($dbh,$this->usuario, $this->senha,$opcoes);
             } catch (PDOException $e) {
                 print "Error!: ".$e->getMenssage()."<br/>";
@@ -23,5 +24,40 @@ class Database {
             }//fim do catch
         ];
     }//fim do construtor
+
+    //prepare Statements com query
+    public function query($sql){
+        //prepare uma consulta sql
+        $this->stmt = $this->dbh->prepare($sql);
+    }//fim da função query
+
+    //vincula um valor ao parametro
+    public function bind($parametro, $valor, $tipo = null){
+        if(is_null($tipo)){
+            switch(true){
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                    break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                    break;
+                case is_null($valor):
+                    $tipo - PDO::PARAM_NULL;
+                    break;
+                default:
+                    $tipo = PDO::PARAM_STR;
+            }//fim do switch
+        }//fim da função bind
+        endif;
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }//fim da funcao bind
+
+    //executa prepared statement
+    public function executa(){
+        return $this->stmt->executa()
+    }//fim da função executa
+
+
+
 }
 ?>
